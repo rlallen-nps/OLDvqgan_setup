c1f3a2b97de8e380e550348c3313cabcf1d998c3
rlallen-nps <54482942+rlallen-nps@users.noreply.github.com>
2021-09-10 09:46:09 -0500

    Create vqgan_setup

---
+++ vqgan_setup.sh
@@ -0,0 +1,22 @@
+# Make executable with chmod +x vqgan_setup.sh
+
+CURRENTDIR=${pwd}
+
+# This installs the CUDA version of Pytorch
+pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+# Install other required Python packages:
+pip install ftfy regex tqdm omegaconf pytorch-lightning IPython kornia imageio imageio-ffmpeg einops torch_optimizer
+
+#Clone required repositories:
+git clone 'https://github.com/nerdyrodent/VQGAN-CLIP'
+cd "VQGAN-CLIP"
+git clone 'https://github.com/openai/CLIP'
+git clone 'https://github.com/CompVis/taming-transformers'
+
+mkdir "checkpoints"
+
+curl -L -o checkpoints/vqgan_imagenet_f16_16384.yaml -C - 'https://heibox.uni-heidelberg.de/d/a7530b09fed84f80a887/files/?p=%2Fconfigs%2Fmodel.yaml&dl=1' #ImageNet 16384
+curl -L -o checkpoints/vqgan_imagenet_f16_16384.ckpt -C - 'https://heibox.uni-heidelberg.de/d/a7530b09fed84f80a887/files/?p=%2Fckpts%2Flast.ckpt&dl=1' #ImageNet 16384
+
+echo "Done"
